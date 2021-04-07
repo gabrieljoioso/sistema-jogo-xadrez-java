dbd8a65a1a690ae57ca952276231b014c9c605b4
Gabriel Joioso <gjoioso.cs@gmail.com>
2021-04-07 14:33:22 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,12 +33,17 @@ public class Board {
 		this.columns = columns;
 	}
 	
-	public Piece piece(int row, int column) {  // retorn a matriz pieces na linha row e coluna column
+	public Piece piece(int row, int column) {  // retorna a matriz pieces na linha row e coluna column
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position) { // sobrecarga
 		return pieces[position.getRow()][position.getColumn()];
+	}
+	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece; // Matriz declarada no tabuleiro e instanciada no construtor. Atribui a peça na posição linha/coluna da matriz. 
+		piece.position = position; // Posição não é mais nula.
 	}
 	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,6 +3,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -10,6 +13,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8, 8); // Tabuleiro 8x8
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() { // Percorre a matriz de peças do board e para cada peça fazemos um downcasting para ChessPiece.
@@ -17,8 +21,14 @@ public class ChessMatch {
 		for	(int i=0; i<board.getRows(); i++) {
 			for (int j=0; j<board.getColumns(); j++) {
 				mat[i][j] = (ChessPiece) board.piece(i, j); // para cada posição ij do tabuleiro minha matriz mat 
-			}												// receba board.piece ij com downcasting para ChessPiece, interpretando como peça de xadrez e não como uma peça comum.
+			}	// receba board.piece ij com downcasting para ChessPiece, interpretando como peça de xadrez e não como uma peça comum.
 		}
 		return mat;
+	}
+	
+	private void initialSetup() { //Iniciar a partida colocando as peças no tabuleiro.
+		board.placePiece(new Rook(board,  Color.WHITE), new Position(2, 1)); // Instancia da peça no tabuleiro com cor branca e posição 2,1.
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 }
